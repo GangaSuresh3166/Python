becde2586151274cddf25cc6371fe2efb031e7e7
Ganga <gangasuresh3166@gmail.com>
2025-06-17 22:38:36 +0530

    modified

--- string.py
+++ string.py
@@ -1,6 +1,5 @@
 text=input("Enter a string:")
 if text=="python":
     print("The entered string is python")
-else:
-    print("The entered string is not python")
-    
+
+print("The entered string is not python")
