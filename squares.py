09e3ef857476461d2924545fb75c2143d2ec3070
Ganga <gangasuresh3166@gmail.com>
2025-06-20 10:31:08 +0530

    changes added

---
+++ squares.py
@@ -0,0 +1,7 @@
+num=[4,2,6,7,3,5,8,10,6,1,9,2]
+square=0
+squares=[]
+for value in num:
+    square=value**2
+    squares.append(square)#append:create cheytha empty list lek value add akkan use cheyyunn
+print("The list of squares is",squares)
