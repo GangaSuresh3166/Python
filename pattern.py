496a07edb0cbbffa016dc5e346d1ecbb3285c13c
Ganga <gangasuresh3166@gmail.com>
2025-06-23 15:37:28 +0530

    changed

--- pattern.py
+++ pattern.py
@@ -9,13 +9,13 @@
 
 # 2.left half pyramid:
 
-n=5
-for i in range(0,n):
-    for j in range(0,n-i):
-        print(" ",end=" ")
-    for k in range(0,i+1):
-        print("*",end=" ")
-    print()
+# n=5
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print(" ",end=" ")
+#     for k in range(0,i+1):
+#         print("*",end=" ")
+#     print()
 
 # 3.full pyramid:
 
@@ -43,6 +43,34 @@ for i in range(0,n):
 #         print("* ",end=" ")
 #     print()
 
+# 6.inverted full pyramid:
+
+# n=5
+# for i in range(0,n):
+#     for j in range(0,n-i):
+#         print(" ",end=" ")
+#     for k in range(0,i+1):
+#         print("* ",end=" ")
+    
+#     print()
 
 
+#14. floyd's triangle:
+
+# n=4
+# a=1
+# for i in range(0,n):
+#     for j in range(0,i+1):
+#         print(a ,end=" ")
+#         a=a+1
+#     print()
+
+# 7. Rhombus pattern
+
+n=5
+for i in range(0,5):
+    for j in range(n,i-1):
+        print("*",end=" ")
+    
+    print()
 
--- stringrev.py
+++ stringrev.py
@@ -1,6 +1,6 @@
 text=(input("Enter the string:"))
 reverse=""
-for i in range text:
+for i in text:
     reverse=i+reverse 
     #reverse =text[::-1] without builtin
 print("reversed string:",reverse)
