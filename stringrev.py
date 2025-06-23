07b8b524b5d8c6b16f05c81b697086a1f4f0392e
Ganga <gangasuresh3166@gmail.com>
2025-06-23 10:31:44 +0530

    patterns

--- stringrev.py
+++ stringrev.py
@@ -1,13 +1,7 @@
-
-s = input("Enter a string: ")
-b = len(s)
-
-j = ""
-i = b - 1
-
-while i >= 0:
-    j += s[i]
-    i -= 1
-
-print("Reversed string:", j)
+text=(input("Enter the string:"))
+reverse=""
+for i in range text:
+    reverse=i+reverse 
+    #reverse =text[::-1] without builtin
+print("reversed string:",reverse)
 
